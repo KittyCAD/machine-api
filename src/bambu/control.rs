199bef0dfcf0b6d06d178eadf35e42be710097b6
Paul R. Tagliamonte <paul@zoo.dev>
2024-08-08 16:08:03 -0400

    more hackery

--- src/bambu/control.rs
+++ src/bambu/control.rs
@@ -1,42 +1,18 @@
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, sync::Arc};
+use bambulabs::client::Client;
+use std::sync::Arc;
 
-/// The configuration for bambu labs machines.
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Config {
-    /// The machine ids and access codes for communication of LAN.
-    pub machines: Vec<MachineConfig>,
-}
-
-impl Config {
-    /// Get the access code for a machine.
-    pub fn get_access_code(&self, id: &str) -> Option<String> {
-        self.machines.iter().find(|m| m.id == id).map(|m| m.access_code.clone())
-    }
-
-    // Get the machine config for the given id.
-    pub fn get_machine_config(&self, id: &str) -> Option<&MachineConfig> {
-        self.machines.iter().find(|m| m.id == id)
-    }
-}
-
-/// The configuration for a single bambu labs machine.
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct MachineConfig {
-    /// The machine id.
-    pub id: String,
-    /// The access code for the machine.
-    pub access_code: String,
-    /// The slicer configuration for the machine.
-    pub slicer_config: PathBuf,
-}
-
+/// Control channel handle to a Bambu Labs X1 Carbon.
 pub struct X1Carbon {
-    pub client: Arc<bambulabs::client::Client>,
+    client: Arc<Client>,
 }
 
 impl X1Carbon {
+    /// Return a borrow of the underlying Client.
+    pub fn inner(&self) -> &Client {
+        self.client.as_ref()
+    }
+
     /// Get the latest status of the printer.
     pub fn get_status(&self) -> Result<Option<bambulabs::message::PushStatus>> {
         self.client.get_status()
--- src/bambu/mod.rs
+++ src/bambu/mod.rs
@@ -1,3 +1,38 @@
 //! This module contains support for printing to Bambu Lab 3D printers.
 
 mod control;
+
+pub use control::X1Carbon;
+
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+/// The configuration for bambu labs machines.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Config {
+    /// The machine ids and access codes for communication of LAN.
+    pub machines: Vec<MachineConfig>,
+}
+
+impl Config {
+    /// Get the access code for a machine.
+    pub fn get_access_code(&self, id: &str) -> Option<String> {
+        self.machines.iter().find(|m| m.id == id).map(|m| m.access_code.clone())
+    }
+
+    /// Get the machine config for the given id.
+    pub fn get_machine_config(&self, id: &str) -> Option<&MachineConfig> {
+        self.machines.iter().find(|m| m.id == id)
+    }
+}
+
+/// The configuration for a single bambu labs machine.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct MachineConfig {
+    /// The machine id.
+    pub id: String,
+    /// The access code for the machine.
+    pub access_code: String,
+    /// The slicer configuration for the machine.
+    pub slicer_config: PathBuf,
+}
