72d565da557c4f0cd615356e3759a4260fdfcc58
Paul R. Tagliamonte <paul@zoo.dev>
2024-08-13 14:50:22 -0400

    AnySlicer enum

--- src/slicer/mod.rs
+++ src/slicer/mod.rs
@@ -4,3 +4,37 @@
 
 pub mod orca;
 pub mod prusa;
+
+use crate::{DesignFile, TemporaryFile};
+use anyhow::Result;
+
+/// All Slicers that are supported by the machine-api.
+pub enum AnySlicer {
+    /// Prusa Slicer
+    Prusa(prusa::Slicer),
+
+    /// Orca Slicer
+    Orca(orca::Slicer),
+}
+
+impl From<prusa::Slicer> for AnySlicer {
+    fn from(slicer: prusa::Slicer) -> Self {
+        Self::Prusa(slicer)
+    }
+}
+
+impl From<orca::Slicer> for AnySlicer {
+    fn from(slicer: orca::Slicer) -> Self {
+        Self::Orca(slicer)
+    }
+}
+
+impl AnySlicer {
+    /// Generate gcode from some input file.
+    pub async fn generate(&self, design_file: &DesignFile) -> Result<TemporaryFile> {
+        match self {
+            Self::Prusa(slicer) => slicer.generate(design_file).await,
+            Self::Orca(slicer) => slicer.generate(design_file).await,
+        }
+    }
+}
