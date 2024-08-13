9c296015929c265a351540c6b63021e83779dc73
Paul R. Tagliamonte <paul@zoo.dev>
2024-08-13 14:02:36 -0400

    gcode

--- src/gcode/mod.rs
+++ src/gcode/mod.rs
@@ -21,7 +21,7 @@ where
     WriteT: Unpin,
 {
     /// Create a new [Client] using some underlying [tokio::io::AsyncWrite].
-    pub fn new(write: WriteT) -> Result<Self> {
+    pub async fn new(write: WriteT) -> Result<Self> {
         Ok(Self { write })
     }
 
@@ -55,3 +55,103 @@ where
         Pin::new(&mut self.write).poll_shutdown(cx)
     }
 }
+
+#[allow(unused_macros)]
+macro_rules! gcode_machine {
+    ($name:ident) => {
+        /// gcode based Control interface to a Machine.
+        pub struct $name<WriteT>
+        where
+            WriteT: AsyncWrite,
+            WriteT: Unpin,
+        {
+            client: std::sync::Arc<tokio::sync::Mutex<$crate::gcode::Client<WriteT>>>,
+        }
+
+        impl<WriteT> $name<WriteT>
+        where
+            WriteT: AsyncWrite,
+            WriteT: Unpin,
+        {
+            /// Create a new wrapper around a Client, with some extra bits.
+            pub(crate) fn from_client(client: $crate::gcode::Client<WriteT>) -> Self {
+                Self {
+                    client: std::sync::Arc::new(tokio::sync::Mutex::new(client)),
+                }
+            }
+
+            /// Return the inner client to directly interface with the
+            /// machine.
+            pub async fn inner(&mut self) -> tokio::sync::MutexGuard<'_, $crate::gcode::Client<WriteT>> {
+                self.client.lock().await
+            }
+        }
+    };
+}
+#[allow(unused_imports)]
+pub(crate) use gcode_machine;
+
+#[cfg(feature = "serial")]
+mod usb {
+    use super::*;
+    use crate::{Control as ControlTrait, MachineInfo as MachineInfoTrait, MachineMakeModel, MachineType, Volume};
+    use tokio_serial::{SerialPortBuilder, SerialPortBuilderExt, SerialStream};
+
+    gcode_machine!(Usb);
+
+    /// USB information
+    #[derive(Debug, Clone, Copy)]
+    pub struct UsbMachineInfo {}
+
+    impl MachineInfoTrait for UsbMachineInfo {
+        type Error = anyhow::Error;
+
+        fn machine_type(&self) -> MachineType {
+            MachineType::FusedDeposition
+        }
+
+        fn make_model(&self) -> MachineMakeModel {
+            MachineMakeModel {
+                manufacturer: None,
+                model: None,
+                serial: None,
+            }
+        }
+
+        fn max_part_volume(&self) -> Result<Volume> {
+            unimplemented!()
+        }
+    }
+
+    impl<WriteT> ControlTrait for Usb<WriteT>
+    where
+        WriteT: AsyncWrite,
+        WriteT: Unpin,
+    {
+        type Error = anyhow::Error;
+        type MachineInfo = UsbMachineInfo;
+
+        async fn machine_info(&self) -> Result<UsbMachineInfo> {
+            unimplemented!();
+        }
+
+        async fn emergency_stop(&self) -> Result<()> {
+            self.client.lock().await.emergency_stop().await
+        }
+
+        async fn stop(&self) -> Result<()> {
+            self.client.lock().await.stop().await
+        }
+    }
+
+    impl Usb<SerialStream> {
+        /// Open a serial port.
+        pub async fn open(builder: SerialPortBuilder) -> Result<Self> {
+            let client = builder.open_native_async()?;
+            Ok(Usb::from_client(Client::new(client).await?))
+        }
+    }
+}
+
+#[cfg(feature = "serial")]
+pub use usb::{Usb, UsbMachineInfo};
